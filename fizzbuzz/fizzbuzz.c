f0b69496b831d3076abb808db6f6994d4d7445f1
James Taranto <taranto.james@gmail.com>
2015-11-24 22:27:24 +1100

    fizzbuzz in c

---
+++ fizzbuzz/fizzbuzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void fizzbuzz(int x, int y, int n)
+{
+	int i;
+
+	for (i = 1; i < n; i++) {
+		if (!(i % x) && !(i % y)) {
+			printf("FB ");
+		} else if (!(i % x)) {
+			printf("F ");
+		} else if (!(i % y)) {
+			printf("B ");
+		} else {
+			printf("%d ", i);
+		}
+	}
+
+	return;
+}
+
+int main(int argc, char **argv)
+{
+	int i, x, y, n;
+	FILE *f;
+
+
+	f = fopen(argv[1], "r");
+	while(fscanf(f, "%d %d %d", &x, &y, &n) != EOF) {
+		fizzbuzz(x, y, n);
+		putchar('\n');
+	}
+	
+	fclose(f);
+
+	return 0;
+}
